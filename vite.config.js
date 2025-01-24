58000c7f97b9ea26e1c1d603d18449a952af3318
JohnCarl <johncarl@nabepero.co.jp>
2025-01-24 16:50:21 +0800

    test: sentry github workflows integration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [],
+    plugins: [
+      sentryVitePlugin({
+        authToken: process.env.VITE_SENTRY_AUTH_TOKEN, // Use `process.env` for compatibility
+        org: process.env.VITE_SENTRY_ORG,
+        project: process.env.VITE_SENTRY_PROJECT,
+        authToken: process.env.VITE_SENTRY_AUTH_TOKEN, // Use `process.env` for compatibility
+    })],
     build: {
         sourcemap: true,
     },
     css: {
         devSourcemap: true,
-    }
+    },
 });
