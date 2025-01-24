27de842d961623edd19f8ddb65a24f6ecb2cac30
JohnCarl <johncarl@nabepero.co.jp>
2025-01-24 17:07:50 +0800

    fi: remove unnecessary

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [
-      sentryVitePlugin({
-        authToken: process.env.VITE_SENTRY_AUTH_TOKEN, // Use `process.env` for compatibility
-        org: process.env.VITE_SENTRY_ORG,
-        project: process.env.VITE_SENTRY_PROJECT,
-        authToken: process.env.VITE_SENTRY_AUTH_TOKEN, // Use `process.env` for compatibility
+    plugins: [sentryVitePlugin({
+      org: process.env.VITE_SENTRY_ORG,
+      project: process.env.VITE_SENTRY_PROJECT,
+      authToken: process.env.VITE_SENTRY_AUTH_TOKEN, // Use `process.env` for compatibility
+  }), sentryVitePlugin({
+        org: "john-carl",
+        project: "vite-test"
     })],
     build: {
         sourcemap: true,
