6527f565d982521ac03bdb47e51a2f1a753f314f
JohnCarl <johncarl@nabepero.co.jp>
2025-02-07 12:03:44 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
+
+// Load environment variables from the custom .env.frontend file
+dotenv.config({ path: '.env.frontend' });
 
 export default defineConfig({
     plugins: [sentryVitePlugin({
-      org: process.env.VITE_SENTRY_ORG,
-      project: process.env.VITE_SENTRY_PROJECT,
-      authToken: process.env.VITE_SENTRY_AUTH_TOKEN, // Use `process.env` for compatibility
-  }), sentryVitePlugin({
         org: "john-carl",
-        project: "vite-test"
+        project: "vite-test",
+        authToken: process.env.VITE_SENTRY_AUTH_TOKEN,
     })],
     build: {
         sourcemap: true,
